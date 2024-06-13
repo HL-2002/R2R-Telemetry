20fb431ec529934b8b203e1f29f01c9b3d8641dc
HL-2002 <henryslang8231682@gmail.com>
2024-06-13 11:01:12 -0400

    Test reader API (generator)

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 // App module controls the app's lifecycle
 // BrowserWindow module creates and manages application windows
-const { app, BrowserWindow } = require('electron')
+// OPTIMIZATION: Needs optimization with import instead of require()
+const { app, BrowserWindow, ipcMain } = require('electron') 
+const path = require('node:path')
+const read = require('./functions/reader.js')
 
 // Function to create a new window
 function createWindow () {
   const window = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+        preload: path.join(__dirname, 'preload.js')
+    }
   })
 
   window.loadFile('front/index.html')
@@ -22,6 +28,12 @@ app.whenReady().then(() => {
             createWindow()
         }
     })
+
+    // Test log
+    console.log("Hello from main.js")
+
+    // Test read API
+    console.log(read())
 })
 
 // Quit app when windows are closed on Windows and Linux
@@ -30,3 +42,6 @@ app.on('window-all-closed', () => {
         app.quit()
     }
 })
+
+// Test messaging between main and renderer processes
+// Main to Renderer
