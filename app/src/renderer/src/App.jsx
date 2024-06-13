9c599f099086f4abc4f09e4324a46216c86ad303
Luis <siul210704@gmail.com>
2024-06-13 17:39:17 -0400

    initial

--- app/src/renderer/src/App.jsx
+++ app/src/renderer/src/App.jsx
@@ -1,16 +1,7 @@
-import Button from './components/Button'
-
 function App() {
   return (
     <>
-      <Button
-        name={'iniciar sistema'}
-        click={() => {
-          // open video youtube joke
-          window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
-        }}
-      />
-      <Button type="error" name={'borrar todo'} />
+    <h1>electron app</h1>
     </>
   )
 }
