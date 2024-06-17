f057d84f1f4feecea5b2918ac1b37753eb56d089
Luis <siul210704@gmail.com>
2024-06-17 19:47:14 -0400

    add new button

--- app/src/renderer/src/App.jsx
+++ app/src/renderer/src/App.jsx
@@ -1,5 +1,5 @@
 import Versions from './components/Versions.jsx'
-import {Line} from 'react-chartjs-2'
+import { Line } from 'react-chartjs-2'
 import { useState } from 'react'
 
 // TODO: Need to import necessary modules for Chart.js only
@@ -7,15 +7,17 @@ import Chart from 'chart.js/auto'
 
 // Test: Importing and using collection
 import RTCollection from './components/RTCollection.jsx'
+import SessionSelection from './components/SessionSelection.jsx'
 
 function App(props) {
   return (
-    <>
-    <h1>electron app</h1>
+    <div>
+      <SessionSelection />
+      <h1>electron app</h1>
     <Line data={props.data}/>
     <RTCollection data={props.data} frequency={props.frequency}/>
     <Versions />
-    </>
+    </div>
   )
 }
 
