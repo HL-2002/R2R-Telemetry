02efd74b1855b50f9be96eee00103a69438625f4
Luis <siul210704@gmail.com>
2024-06-19 17:18:00 -0400

    add close btn

--- app/src/renderer/src/components/Dialog.jsx
+++ app/src/renderer/src/components/Dialog.jsx
@@ -4,9 +4,21 @@ export default function Dialog({ children, isOpen, someRef }) {
       ref={someRef}
       className="p-6 rounded 
     m-auto
-    bg-[#22272b]"
+    bg-[#22272b]
+    relative
+    "
       open={isOpen}
     >
+      <button
+        onClick={() => {
+          someRef.current.close()
+        }}
+        className="absolute right-[0.5rem] top-[0.5rem] top py-1 px-2 bg-[#ec6d2d] text-[#dee4ea] rounded text-xs
+      hover:bg-[#e94926] hover:text-[#dee4ea] transition-colors
+      "
+      >
+        X
+      </button>
       {children}
     </dialog>
   )
