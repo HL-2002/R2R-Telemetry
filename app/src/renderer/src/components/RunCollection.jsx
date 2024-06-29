de457d991e5d2df28e099fcdb5a6154c1788c90b
Luis <siul210704@gmail.com>
2024-06-29 16:47:11 -0400

    use session store to show runs

--- app/src/renderer/src/components/RunCollection.jsx
+++ app/src/renderer/src/components/RunCollection.jsx
@@ -1,27 +1,20 @@
+import { useSessionStore } from '../context/SessionContext'
 import Run from './Run'
 
 export default function RunCollection({ mode }) {
+  const runs = useSessionStore((state) => state.Runs)
   return (
     <div
+      // why you make this with inline styles? you can use tailwindcss classes
       style={{ height: 75 + 'vh', overflowY: 'scroll', overflowX: 'hidden' }}
       className="rounded-lg
                         bg-[#282e33]
                         p-2
                         flex-grow"
     >
-      <Run mode={mode} />
-      <Run mode={mode} />
-      <Run mode={mode} />
-      <Run mode={mode} />
-      <Run mode={mode} />
-      <Run mode={mode} />
-      <Run mode={mode} />
-      <Run mode={mode} />
-      <Run mode={mode} />
-      <Run mode={mode} />
-      <Run mode={mode} />
-      <Run mode={mode} />
-      <Run mode={mode} />
+      {runs.map((run, index) => (
+        <Run key={run.id} index={index} run={run} mode={mode} />
+      ))}
     </div>
   )
 }
