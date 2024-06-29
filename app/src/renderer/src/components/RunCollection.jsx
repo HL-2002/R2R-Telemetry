b2cf83b9c808135582fb7eb4f4474217ec9ed760
Luis <siul210704@gmail.com>
2024-06-29 09:44:28 -0400

    format code

--- app/src/renderer/src/components/RunCollection.jsx
+++ app/src/renderer/src/components/RunCollection.jsx
@@ -1,25 +1,27 @@
 import Run from './Run'
 
-export default function RunCollection ({mode}) {
-    return (
-        <div style={{height: 75 + 'vh', overflowY: 'scroll', overflowX: 'hidden'}}
-                className='rounded-lg
+export default function RunCollection({ mode }) {
+  return (
+    <div
+      style={{ height: 75 + 'vh', overflowY: 'scroll', overflowX: 'hidden' }}
+      className="rounded-lg
                         bg-[#282e33]
                         p-2
-                        flex-grow'>
-            <Run mode={mode}/>
-            <Run mode={mode}/>
-            <Run mode={mode}/>
-            <Run mode={mode}/>
-            <Run mode={mode}/>
-            <Run mode={mode}/>
-            <Run mode={mode}/>
-            <Run mode={mode}/>
-            <Run mode={mode}/>
-            <Run mode={mode}/>
-            <Run mode={mode}/>
-            <Run mode={mode}/>
-            <Run mode={mode}/>
-        </div>
-    )
+                        flex-grow"
+    >
+      <Run mode={mode} />
+      <Run mode={mode} />
+      <Run mode={mode} />
+      <Run mode={mode} />
+      <Run mode={mode} />
+      <Run mode={mode} />
+      <Run mode={mode} />
+      <Run mode={mode} />
+      <Run mode={mode} />
+      <Run mode={mode} />
+      <Run mode={mode} />
+      <Run mode={mode} />
+      <Run mode={mode} />
+    </div>
+  )
 }
