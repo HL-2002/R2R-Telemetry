44de99cb934ce2077b9243bdc4af496b70d2c10e
Luis <siul210704@gmail.com>
2024-06-29 16:46:56 -0400

    use index of run

--- app/src/renderer/src/components/Run.jsx
+++ app/src/renderer/src/components/Run.jsx
@@ -1,7 +1,8 @@
-export default function Run ({mode}) {
-
+export default function Run({ mode, index, run }) {
+//   run have a atribute duration xd use it
     return (
-        <button className="m-1 p-2
+    <button
+      className="m-1 p-2
                         bg-[#e94926]
                         rounded-md
                         text-[#dee4ea]
@@ -9,15 +10,15 @@ export default function Run ({mode}) {
                         shadow-slate-900
                         hover:bg-[#ec6d2d]
                         disabled:opacity-50"
-                style={{width: 95 + '%'}}
-                onClick={() => {}}
-                disabled={mode !== 'read'}> 
-            <h1 className="text-left">Run #a</h1>  
-            <div className='flex justify-between'>
-                <p className="text-sm"> HH:MM </p>
-                <p className="text-sm"> MM:SS:MSS </p>
-            </div>
-        </button>
-    )
-        
+      style={{ width: 95 + '%' }}
+      onClick={() => {}}
+      disabled={mode !== 'read'}
+    >
+      <h1 className="text-left">Run {index}#</h1>
+      <div className="flex justify-between">
+        <p className="text-sm"> HH:MM </p>
+        <p className="text-sm"> MM:SS:MSS </p>
+      </div>
+    </button>
+  )
 }
