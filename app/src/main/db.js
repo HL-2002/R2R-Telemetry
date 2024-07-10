c2ce5553ae229454e6435ddc85177a8b2245c838
Luis <siul210704@gmail.com>
2024-07-09 22:16:20 -0400

    erase all relative to client

--- app/src/main/db.js
+++ app/src/main/db.js
@@ -4,37 +4,7 @@ export const db = createClient({
   url: 'file:./tele.db'
 })
 
-// FOR SUICIDED
 db.execute('pragma foreign_keys = ON')
-
-// create table client
-db.execute(`CREATE TABLE IF NOT EXISTS client (
-  cedula INTEGER PRIMARY KEY,
-  name TEXT,
-  lastname TEXT,
-  vehicle TEXT,
-  address TEXT
-)`)
-
-// create table tax
-
-db.execute(`CREATE TABLE IF NOT EXISTS tax (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  description REAL,
-  percentage REAL
-)`)
-
-// create table bill
-
-db.execute(`CREATE TABLE IF NOT EXISTS bill (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  total INTEGER,
-  cost REAL,
-  id_tax INTEGER,
-  id_session INTEGER,
-  FOREIGN KEY(id_tax) REFERENCES tax(id),
-  FOREIGN KEY(id_session) REFERENCES session(id) ON DELETE CASCADE
-)`)
 
 // FOR R2R
 db.execute(`CREATE TABLE IF NOT EXISTS session (
@@ -42,9 +12,7 @@ db.execute(`CREATE TABLE IF NOT EXISTS session (
   description TEXT,
   type TEXT,
   date TEXT,
-  time TEXT,
-  client_id INTEGER,
-  FOREIGN KEY(client_id) REFERENCES client(cedula) ON DELETE CASCADE
+  time TEXT
 )`)
 
 // create table run
