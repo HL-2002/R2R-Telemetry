73d346a004c380a44909b4640b236c9a91ad940d
Luis <siul210704@gmail.com>
2024-07-12 09:03:47 -0400

    add default graph

--- app/src/renderer/src/context/SelectionContext.js
+++ app/src/renderer/src/context/SelectionContext.js
@@ -1,4 +1,6 @@
 import { create } from 'zustand'
+import constants from '../constants'
+const { AllgraphSafe } = constants
 
 // create a store for the selection context
 const useSelectionStore = create((set) => ({
@@ -6,7 +8,7 @@ const useSelectionStore = create((set) => ({
 
   // selections is an array of objects that represent the selected data points
   selections: [],
-  safeSelections: [],
+  safeSelections: [...AllgraphSafe],
   setSelection: (selection) => set({ selections: selection }),
   setSafeSelection: (selection) => set({ safeSelections: selection }),
   // Axis is the axis that the user is currently selecting
